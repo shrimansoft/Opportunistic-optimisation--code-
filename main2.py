d1ab6e050fec99679c4471140b7ee1919f13b3e4
Shriman Keshri <shrimansoft@hotmail.com>
2024-10-31 01:47:54 +0530

    make a class werhouse and code is working fine.

--- main2.py
+++ main2.py
@@ -1,169 +1,144 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# %% 
-itemTypes = np.arange(0,50)
-print(itemTypes)
-print(itemTypes.dtype)
+class Robot():
 
-stock = np.ones(50)*48
-print(stock)
-print(stock.dtype)
+    def __init__(self):
+        self.available = True
+        self.time_left = 0  # time left in the task.
+        self.shelf = None  # which self is above it
 
-itemBuffer = np.zeros(50)
-print(itemBuffer)
+    def assigne(self, shelf, distance):
+        self.shelf = shelf
+        self.time_left = distance
+        self.available = False
+        print("start>> \t", self.shelf, "distanc:\t", self.time_left)
 
-#shelfs (but not using this)
-itemShelfsBuffer = [[]]*50
-
-#shelfs (but not using this)
-shelfs = np.empty(400,dtype=object)
-shelfs[:] = [[]]
-print(shelfs)
-
-
-
-
-# fill the wear house
-items = np.repeat(np.arange(0,50),48)
-print(items)
-
-np.random.shuffle(items)
-print(items)
-
-shelfs = items.reshape(400,6)
-shelfs = shelfs.tolist()
-shelfs
+    def step(self, itemShelfsBuffer, itemBuffer):
+        if self.time_left == 0 and (not self.available):
+            order_count = 0
+            print(">>> ", self.shelf)
+            print()
+            for i, itemShelfs in enumerate(itemShelfsBuffer):
+                for itemShelf in itemShelfs:
+                    if itemShelf == self.shelf:
+                        itemShelfs.remove(self.shelf)
+                        itemBuffer[i] -= 1
+                        order_count += 1
+            print("stop>> \t", self.shelf, "order:\t", order_count)
+            self.available = True
+            self.shelf = None
+        if self.time_left > 0:
+            self.time_left -= 1
 
 
+class Wearhouse():
 
-#filling the wearhouse with random itesm (order history)
-probabilities = np.random.dirichlet(np.ones(50), size=1)[0]
+    def __init__(self):
+        self.stock = np.ones(50) * 48
+        self.probabilities = np.random.dirichlet(np.ones(50), size=1)[0]
+        self.itemBuffer = np.zeros(50)  # don't use it.
+        self.itemShelfsBuffer = [[]] * 50
+        self.robots = [Robot(), Robot()]
+        self.distance = np.array([((i % 20) + (i // 20) + 2)
+                                  for i in range(400)])
 
-plt.barh(range(50),probabilities)
-plt.show()
-print(probabilities)
+        # fill the wear house
+        items = np.repeat(np.arange(0, 50), 48)
+        np.random.shuffle(items)
+        shelfs = items.reshape(400, 6)
+        self.shelfs = shelfs.tolist()
+        ...
 
+    def sample(self):
+        return int(
+            np.random.choice(np.arange(50), size=1, p=self.probabilities))
 
-# sample the oreder randomly. 
+    def available(self):
+        return list(map(bool, self.stock))
 
-samples = np.random.choice(np.arange(50), size=10, p=probabilities)
-print(samples)
+    def itemInShelfs(self, n):
+        """TODO describe function
 
+        :param n: item number
+        :returns: a list showing which shelf have how much item n. 
 
-# orde only for stock
+        """
+        return list(map(lambda x: sum([1 for i in x if i == n]), self.shelfs))
 
+    def nearestShelf(self, n):
+        availableInShelfs = list(map(bool, self.itemInShelfs(n)))
+        filteredList = [
+            (i, v)
+            for i, (v, l) in enumerate(zip(self.distance, availableInShelfs))
+            if l
+        ]
+        shelf, distence = min(filteredList, key=lambda x: x[1])
+        return shelf, distence
 
-print(stock)
+    def order_step(self):
+        if (np.random.random() < 0.3):
+            available = self.available()
+            samples = self.sample()
+            if available[samples]:
+                self.itemBuffer[samples] += 1
+                shelf, distence = self.nearestShelf(samples)
 
-stock[1:30] = 0
+                self.itemShelfsBuffer[samples].append(shelf)
+                self.shelfs[shelf].remove(samples)
 
-available = list(map(bool,stock))
-
-print(probabilities*available)
-
-
-
-# shelf distance
-distance = np.array([((i%20)+(i//20)+2) for i in range(400)])
-print(distance)
-
-# shelf buffer
-
-
-
-def itemInShelfs(n):
-    return list(map(lambda x:sum([1 for i in x if i == n]), shelfs))
-
-
-def nearestShelf(n):
-    availableInShelfs= list(map(bool,itemInShelfs(n)))
-    # print(availableInShelfs)
-
-    filteredList = [(i,v) for i,(v,l) in enumerate(zip(distance,availableInShelfs)) if l]
-    # print(filteredList)
-
-    shelf,distence = min(filteredList,key=lambda x: x[1])
-    # print(shelf)
-    return shelf,distence
-
-print(nearestShelf(9))
-
-print(shelfs)
-# %%
-# while True:
-
-robot_is_available = True
-robot_time_left_in_the_task = 0
-robot_shelf = None
-t=0
-
-# for t in range(100):
-while True:
-    if(np.random.random()<0.3):
-        available = list(map(f,stock))
-        samples = int(np.random.choice(np.arange(50), size=1, p=probabilities ))
-        if available[samples]:
-            itemBuffer[samples] += 1
-            shelf,distence = nearestShelf(samples)
-            # print(type(samples))
-            itemShelfsBuffer[samples].append(shelf)
-            shelfs[shelf].remove(samples)
-            # print(itemShelfsBuffer[samples][0])
-            stock[samples] -= 1
-            # print(itemBuffer)
-
-    if robot_time_left_in_the_task == 0 and (not robot_is_available):
-        order_count = 0
-        for i,itemShelfs in enumerate(itemShelfsBuffer):
-            for itemShelf in itemShelfs:
-                if itemShelf == robot_shelf:
-                    itemShelfs.remove(robot_shelf)
-                    itemBuffer[i] -= 1
-                    order_count += 1
-        print("shelf>> \t",robot_shelf,"stoped at \t", t,"order:\t",order_count)
-        robot_is_available =True
-        robot_shelf = None
-    if len(itemShelfsBufferSet) == 0 and stock.sum() == 0:
-        print(i)
-        break
-
-    # order exisution
-    itemShelfsBufferSet =  list(set().union(*itemShelfsBuffer))
-    # select a shelf
-
-    if robot_time_left_in_the_task > 0:
-        robot_time_left_in_the_task -= 1
-
-    if len(itemShelfsBufferSet) > 0 and robot_is_available:
-        shelf_to_move = int(np.random.choice(itemShelfsBufferSet))
-        # print(itemShelfsBufferSet)
-        # print(shelf_to_move)
-        robot_shelf = shelf_to_move
-        robot_is_available = False
-        robot_time_left_in_the_task = 2*distance[shelf_to_move]
-        print("shelf>> \t",robot_shelf," started at \t", t,"distanc:\t",robot_time_left_in_the_task,"total stock\t", stock.sum())
-
-    t+=1
-    # you have one robot
-
-# %%
-
-# itemShelfsBufferSet =  list(set().union(*itemShelfsBuffer))
+                self.stock[samples] -= 1
 
 
+def main():
 
+    wearhouse = Wearhouse()
+    stock = wearhouse.stock
+    itemBuffer = wearhouse.itemBuffer
+    #shelfs (but not using this)
+    itemShelfsBuffer = wearhouse.itemShelfsBuffer
+    shelfs = wearhouse.shelfs
+    #filling the wearhouse with random itesm (order history)
+    probabilities = wearhouse.probabilities
+    # plt.barh(range(50),probabilities)
+    # plt.show()
 
-# print(itemShelfsBuffer)
-# print(itemShelfsBufferSet)
-# print([shelfs[i] for i in itemShelfsBufferSet])
-# print([distance[i] for i in itemShelfsBufferSet])
+    available = wearhouse.available
 
+    distance = wearhouse.distance
 
-# calculate the expected time
+    robot_is_available = wearhouse.robots[0].available
+    robot_time_left_in_the_task = wearhouse.robots[0].time_left
+    robot_shelf = wearhouse.robots[0].shelf
+    t = 0
+
+    while True:
+        # for t in range(100):
+
+        wearhouse.order_step()
+
+        wearhouse.robots[0].step(wearhouse.itemShelfsBuffer,
+                                 wearhouse.itemBuffer)
+
+        itemShelfsBufferSet = list(set().union(*itemShelfsBuffer))
+        if len(itemShelfsBufferSet) == 0 and stock.sum() == 0:
+            print(t)
+            break
+
+        # order exisution
+        itemShelfsBufferSet = list(set().union(*itemShelfsBuffer))
+
+        if len(itemShelfsBufferSet) > 0 and wearhouse.robots[0].available:
+            # print(itemShelfsBufferSet)
+            print("Sthock>> ", stock.sum())
+
+            shelf_to_move = int(np.random.choice(itemShelfsBufferSet))
+
+            wearhouse.robots[0].assigne(shelf_to_move,
+                                        2 * distance[shelf_to_move])
+        t += 1
+
 
 def expectedTime():
     total_time = 0
@@ -175,35 +150,13 @@ def expectedTime():
 
         for item in shelf:
             prob = probabilities[item]
-            time = prob * dist # we are assuming unit speed.
+            time = prob * dist  # we are assuming unit speed.
             count += 1
             total_prob += prob
             total_time += time
             # print(dist)
-    return total_time/total_prob
+    return total_time / total_prob
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
