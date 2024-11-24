6f2206f322153cc50c9ff233fb960c1cdf59847e
shriman <shrimansoft@gmail.com>
2024-11-24 05:41:37 +0530

    Formated the Document.

--- warehouse_sim/robot.py
+++ warehouse_sim/robot.py
@@ -1,60 +1,70 @@
-
-
-
-
 class Robot():
 
-    def __init__(self,warehouse,id):
+    def __init__(self, warehouse, id):
         self.warehouse = warehouse
         self.pickingStation = self.warehouse.picking_stations[0]
         self.robot_id = id
         self.available = True
-        self.mode = 0 # 0: available 1: goint to shelf 2: going to pickup station 3: retruning the shelf to its location.
+        # Mode expanation
+        # 0: available
+        # 1: goint to shelf
+        # 2: going to pickup station
+        # 3: retruning the shelf to its location.
+        self.mode = 0
         self.time_left = 0  # time left in the task.
         self.shelf = None  # which self is above it
-        self.current_location = (0,0)
+        self.current_location = (0, 0)
         self.target_location = self.current_location
         self.shelf_location = None
 
-    def assigne(self, shelf, distance,shelf_location): # (mode 0 -> 1)
+    def assigne(self, shelf, distance, shelf_location):  # (mode 0 -> 1)
         self.mode = 1
         self.shelf = shelf
         self.shelf_location = shelf_location
         self.target_location = shelf_location
-        print("Assignment id done \t",self.current_location,'\t',self.target_location)
+        print("Assignment id done \t", self.current_location, '\t',
+              self.target_location)
         self.time_left = distance
         self.available = False
         print("start>> \t", self.shelf, "distanc:\t", self.time_left)
 
     def step(self):
-        if self.current_location == self.target_location:# Move one step towards the target location
-            if self.mode == 1: # (mode 1 -> 2)
-                self.target_location = self.pickingStation.location # picking station location.
+        # Move one step towards the target location
+        if self.current_location == self.target_location:
+            if self.mode == 1:  # (mode 1 -> 2)
+                # picking station location.
+                self.target_location = self.pickingStation.location
                 self.mode = 2
-            elif self.mode == 2: # (mode 2 -> 3)
+            elif self.mode == 2:  # (mode 2 -> 3)
                 self.target_location = self.shelf_location
                 order_count = 0
                 print(">>> ", self.shelf)
                 print()
-                for i, itemShelfs in enumerate(self.warehouse.itemShelfsBuffer): # looping over types
-                    for itemShelf in itemShelfs: # looping over shelf assigned to a item type.
-                        if itemShelf == self.shelf: # if the shelf is current shelf
+                for i, itemShelfs in enumerate(
+                        self.warehouse.itemShelfsBuffer):  # looping over types
+
+                    # looping over shelf assigned to a item type.
+                    for itemShelf in itemShelfs:
+                        # if the shelf is current shelf
+                        if itemShelf == self.shelf:
                             itemShelfs.remove(self.shelf)
                             self.warehouse.itemBuffer[i] -= 1
                             order_count += 1
+
                 def check_order(order):
                     if order.shelf_aloted == self.shelf:
-                        order.done(self.warehouse.time,self.robot_id)
+                        order.done(self.warehouse.time, self.robot_id)
                         self.warehouse.order_compleated.append(order)
                         return False
                     else:
                         return True
 
-                self.warehouse.order_buffer = list(filter(check_order,self.warehouse.order_buffer))
+                self.warehouse.order_buffer = list(
+                    filter(check_order, self.warehouse.order_buffer))
                 print("stop>> \t", self.shelf, "order:\t", order_count)
                 self.mode = 3
 
-            elif self.mode == 3: # (mode 3 -> 0)
+            elif self.mode == 3:  # (mode 3 -> 0)
                 self.mode = 0
                 self.available = True
                 self.shelf = None
@@ -63,10 +73,13 @@ class Robot():
         if self.time_left > 0:
             self.time_left -= 1
 
-        if self.current_location != self.target_location:# Move one step towards the target location
-            print("robot id \t",self.robot_id, "roblot current location \t", self.current_location)
+        # Move one step towards the target location
+        if self.current_location != self.target_location:
+            print("robot id \t", self.robot_id, "roblot current location \t",
+                  self.current_location)
 
-            # (We assume that target_location is a tuple like (x, y) and current_location is also a tuple (x, y))
+            # (We assume that target_location is a tuple like (x, y)
+            # and current_location is also a tuple (x, y))
             x, y = self.current_location
             print(self.target_location)
             target_x, target_y = self.target_location
